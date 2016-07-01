67188846a4061471aec66d6baba333699578913a
ikedam <devld@ikedam.jp>
2016-07-02 08:59:33 +0900

    [FIXED JENKINS-36398] `assertEqualDataBoundBeans` checks also properties defined with `DataBoundSetter`.

--- src/test/java/org/jvnet/hudson/test/JenkinsRuleTest.java
+++ src/test/java/org/jvnet/hudson/test/JenkinsRuleTest.java
@@ -3,6 +3,7 @@ package org.jvnet.hudson.test;
 import org.junit.Rule;
 import org.junit.Test;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
 
 import java.util.Collections;
 import java.util.List;
@@ -31,6 +32,47 @@ public class JenkinsRuleTest {
 
         public List<String> getSomeList() {
             return someList;
+        }
+    }
+
+    @Test
+    public void assertEqualDataBoundBeansWithSetters() throws Exception {
+        SomeClassWithSetters l = new SomeClassWithSetters("value1");
+        l.setSetterParam("value2");
+        SomeClassWithSetters r = new SomeClassWithSetters("value1");
+        r.setSetterParam("value2");
+        j.assertEqualDataBoundBeans(l, r);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void assertEqualDataBoundBeansWithSettersFail() throws Exception {
+        SomeClassWithSetters l = new SomeClassWithSetters("value1");
+        l.setSetterParam("value2");
+        SomeClassWithSetters r = new SomeClassWithSetters("value1");
+        r.setSetterParam("value3");     // mismatch!
+        j.assertEqualDataBoundBeans(l, r);
+    }
+
+    public static class SomeClassWithSetters {
+        private String ctorParam;
+        private String setterParam;
+
+        @DataBoundConstructor
+        public SomeClassWithSetters(String ctorParam) {
+            this.ctorParam = ctorParam;
+        }
+
+        public String getCtorParam() {
+            return ctorParam;
+        }
+
+        @DataBoundSetter
+        public void setSetterParam(String setterParam) {
+            this.setterParam = setterParam;
+        }
+
+        public String getSetterParam() {
+            return setterParam;
         }
     }
 }
