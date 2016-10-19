0ad1b633a7b9927592c9e5c1a5cdb51eb38952fd
Kristin Whetstone <kwhetstone@cloudbees.com>
2016-10-19 17:36:53 -0400

    Add a feature to record and mock http apis.

---
+++ src/main/java/org/jvnet/hudson/WireMockRuleFactory.java
@@ -0,0 +1,60 @@
+package org.jvnet.hudson.test;
+
+import com.github.tomakehurst.wiremock.common.SingleRootFileSource;
+import com.github.tomakehurst.wiremock.core.Options;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+/**
+ * Provides a way to record and mock REST calls.  The underlying structure is 
+ * <a href="http://wiremock.org/">WireMock</a>.  To use: 
+ * <pre>
+ * {@code
+ * WireMockRuleFactory wmrf = new WireMockRuleFactory(api_to_mock);
+ * @Rule
+ * WireMockRule = wmrf.getRule(8089); //port is configurable  
+ * }
+ * </pre>
+ *
+ * Point your system to "http://localhost:8089/" to see mocked responses.
+ *
+ * To record mocks: {@code mvn clean test -DrecordMocks=true}  Clearing 
+ * previously recorded mocks before generating new ones is recommended. 
+ */
+public class WireMockRuleFactory {
+    private String prop = System.getProperty("toRecord");
+    private String urlToMock;
+
+    /**
+     * @param url  The source to mock through this particular WireMock instance
+     */
+    public WireMockRuleFactory(String url){
+        urlToMock = url;
+    } 
+
+    public WireMockRule getRule(int port){
+        return getRule(wireMockConfig().port(port));
+    }
+
+    public WireMockRule getRule(Options options) {
+        if(prop.equalsIgnoreCase("true")) {
+            return new WireMockRecorderRule(options, urlToMock);
+        } else {
+            return new WireMockRule(options);
+        }
+    }
+
+
+    private class WireMockRecorderRule extends WireMockRule {
+        //needed for WireMockRule file location
+        private String mappingLocation = "src/test/resources";
+
+        public WireMockRecorderRule(Options options, String url){
+            super(options);
+            this.stubFor(get(urlMatching(".*")).atPriority(10).willReturn(aResponse().proxiedFrom(url)));
+            this.enableRecordMappings(new SingleRootFileSource(mappingLocation + "/mappings"), new SingleRootFileSource(mappingLocation + "/__files"));
+        }
+    }
+}
