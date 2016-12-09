a937f95f156a7206f952ae1fa9c2bddeafba9e11
Kristin Whetstone <kwhetstone@cloudbees.com>
2016-12-09 02:47:31 -0500

    Add ability to define custom locations for mocks

--- src/main/java/org/jvnet/hudson/test/WireMockRuleFactory.java
+++ src/main/java/org/jvnet/hudson/test/WireMockRuleFactory.java
@@ -1,15 +1,14 @@
 package org.jvnet.hudson.test;
 
 import com.github.tomakehurst.wiremock.common.SingleRootFileSource;
-import com.github.tomakehurst.wiremock.core.Options;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 /**
- * Provides a way to record and mock REST calls.  The underlying structure is 
- * <a href="http://wiremock.org/">WireMock</a>.  To use: 
+ * Provides a way to record and mock REST calls. The underlying structure is 
+ * <a href="http://wiremock.org/">WireMock</a>. To use: 
  * <pre>
  * {@code
  * WireMockRuleFactory wmrf = new WireMockRuleFactory();
@@ -20,17 +19,27 @@ import static com.github.tomakehurst.wiremock.client.WireMock.*;
  *
  * Point your system to "http://localhost:8089/" to see mocked responses.
  *
- * To record mocks: {@code mvn test -Dwiremock.record="http://api_url"}  Clearing 
+ * To record mocks: {@code mvn test -Dwiremock.record="http://api_url"} Clearing 
  * previously recorded mocks before generating new ones is recommended. 
+ *
+ * The mocks are defaultly written to `src/test/resources`. Use the 2nd 
+ * constructor option to set an alternative location:
+ * <pre>
+ * {@code
+ * WireMockRuleFactory wmrf = new WireMockRuleFactory();
+ * @Rule
+ * WireMockRule wireMockRule = wmrf.getRule("src/test/resources/mockLoc")
+ * }
+ * </pre>
  */
 public class WireMockRuleFactory {
     private String urlToMock = System.getProperty("wiremock.record");
 
     public WireMockRule getRule(int port) {
-        return getRule(wireMockConfig().port(port));
+        return getRule(WireMockConfiguration.wireMockConfig().port(port));
     }
 
-    public WireMockRule getRule(Options options) {
+    public WireMockRule getRule(WireMockConfiguration options) {
         if(urlToMock != null && !urlToMock.isEmpty()) {
             return new WireMockRecorderRule(options, urlToMock);
         } else {
@@ -38,13 +47,34 @@ public class WireMockRuleFactory {
         }
     }
 
+    public WireMockRule getRule(String mapLoc) {
+        return getRule(mapLoc, WireMockConfiguration.wireMockConfig());
+    }
+
+    public WireMockRule getRule(String mapLoc, WireMockConfiguration options) {
+        if(urlToMock != null && !urlToMock.isEmpty()) {
+            return new WireMockRecorderRule(options, urlToMock, mapLoc);
+        } else {
+            return new WireMockRule(options
+               .usingFilesUnderClasspath(mapLoc));
+        }
+    }
+
 
     private class WireMockRecorderRule extends WireMockRule {
-        //needed for WireMockRule file location
         private String mappingLocation = "src/test/resources";
 
-        public WireMockRecorderRule(Options options, String url) {
+        public WireMockRecorderRule(WireMockConfiguration options, String url) {
             super(options);
+            finshWireMockRuleSetup(options, url, this.mappingLocation);
+        }
+
+        public WireMockRecorderRule(WireMockConfiguration options, String url, String mappingLocation) {
+            super(options);
+            finshWireMockRuleSetup(options, url, mappingLocation);
+        }
+
+        private void finshWireMockRuleSetup(WireMockConfiguration options, String url, String mappingLocation) {
             this.stubFor(get(urlMatching(".*")).atPriority(10).willReturn(aResponse().proxiedFrom(url)));
             this.enableRecordMappings(new SingleRootFileSource(mappingLocation + "/mappings"), new SingleRootFileSource(mappingLocation + "/__files"));
         }
