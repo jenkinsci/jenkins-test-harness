04bf07857801b8ea4424d9485c5201a2db9b66d5
Jesse Glick <jglick@cloudbees.com>
2023-08-23 16:18:42 -0400

    Added wait option

--- src/main/java/org/jvnet/hudson/test/RemainingActivityListener.java
+++ src/main/java/org/jvnet/hudson/test/RemainingActivityListener.java
@@ -40,28 +40,45 @@ public final class RemainingActivityListener implements EndOfTestListener {
     /**
      * Set to true if you wish for warnings to be treated as fatal test errors.
      */
-    public static boolean fatal = Boolean.getBoolean(RemainingActivityListener.class.getName() + ".fatal");
+    private static final boolean fatal = Boolean.getBoolean(RemainingActivityListener.class.getName() + ".fatal");
+
+    /**
+     * Set to true if you wish for test shutdown to just wait for activity to cease (not always appropriate).
+     */
+    private static final boolean wait = Boolean.getBoolean(RemainingActivityListener.class.getName() + ".wait");
 
     @Override
     public void onTearDown() throws Exception {
+        if (wait) {
+            String problem;
+            while ((problem = problem()) != null) {
+                Logger.getLogger(RemainingActivityListener.class.getName()).warning(problem);
+                Thread.sleep(5_000);
+            }
+        } else {
+            String problem = problem();
+            if (problem != null) {
+                if (fatal) {
+                    throw new AssertionError(problem);
+                } else {
+                    Logger.getLogger(RemainingActivityListener.class.getName()).warning(problem);
+                }
+            }
+        }
+    }
+
+    private static String problem() {
         for (Computer c : Jenkins.get().getComputers()) {
             for (Executor x : c.getAllExecutors()) {
                 if (!x.isIdle()) {
-                    problem(x.getCurrentExecutable() + " still seems to be running, which could break deletion of log files or metadata");
+                    return x.getCurrentExecutable() + " still seems to be running, which could break deletion of log files or metadata";
                 }
             }
         }
         for (Queue.Item q : Queue.getInstance().getItems()) {
-            problem(q + " is still scheduled, which if it ever runs, could break deletion of log files or metadata");
+            return q + " is still scheduled, which if it ever runs, could break deletion of log files or metadata";
         }
-    }
-
-    private static void problem(String message) {
-        if (fatal) {
-            throw new AssertionError(message);
-        } else {
-            Logger.getLogger(RemainingActivityListener.class.getName()).warning(message);
-        }
+        return null;
     }
 
 }
