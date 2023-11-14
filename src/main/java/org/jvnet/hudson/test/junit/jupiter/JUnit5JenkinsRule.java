07b4015f21efd4e140ab054e545fabf5378c060a
Bryan Stopp <bryan.stopp@gmail.com>
2023-11-14 12:06:59 -0700

    Add static class support.

--- src/main/java/org/jvnet/hudson/test/junit/jupiter/JUnit5JenkinsRule.java
+++ src/main/java/org/jvnet/hudson/test/junit/jupiter/JUnit5JenkinsRule.java
@@ -1,9 +1,10 @@
 package org.jvnet.hudson.test.junit.jupiter;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
+
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import org.junit.jupiter.api.extension.ExtensionContext;
-import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.runner.Description;
 import org.jvnet.hudson.test.JenkinsRecipe;
 import org.jvnet.hudson.test.JenkinsRule;
@@ -12,25 +13,25 @@ import org.jvnet.hudson.test.JenkinsRule;
  * Provides JUnit 5 compatibility for {@link JenkinsRule}.
  */
 class JUnit5JenkinsRule extends JenkinsRule {
-    private final ParameterContext context;
 
-    JUnit5JenkinsRule(@NonNull ParameterContext context, @NonNull ExtensionContext extensionContext) {
-        this.context = context;
+    JUnit5JenkinsRule(@NonNull ExtensionContext extensionContext, Annotation... annotations) {
         this.testDescription = Description.createTestDescription(
-                extensionContext.getTestClass().map(Class::getName).orElse(null),
-                extensionContext.getTestMethod().map(Method::getName).orElse(null));
+            extensionContext.getTestClass().map(Class::getName).orElse(null),
+            extensionContext.getTestMethod().map(Method::getName).orElse(null),
+            annotations);
     }
 
     @Override
     public void recipe() throws Exception {
-        JenkinsRecipe jenkinsRecipe = context.findAnnotation(JenkinsRecipe.class).orElse(null);
-        if (jenkinsRecipe == null) {
+        final JenkinsRecipe recipe = this.testDescription.getAnnotation(JenkinsRecipe.class);
+
+        if (recipe == null) {
             return;
         }
         @SuppressWarnings("unchecked")
         final JenkinsRecipe.Runner<JenkinsRecipe> runner =
-                (JenkinsRecipe.Runner<JenkinsRecipe>) jenkinsRecipe.value().getDeclaredConstructor().newInstance();
+                (JenkinsRecipe.Runner<JenkinsRecipe>) recipe.value().getDeclaredConstructor().newInstance();
         recipes.add(runner);
-        tearDowns.add(() -> runner.tearDown(this, jenkinsRecipe));
+        tearDowns.add(() -> runner.tearDown(this, recipe));
     }
 }
