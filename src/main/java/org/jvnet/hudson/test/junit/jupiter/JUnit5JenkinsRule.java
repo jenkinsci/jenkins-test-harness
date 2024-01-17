a0f648d2b318d80e1ffb7af226b00274c14001d9
Basil Crow <me@basilcrow.com>
2024-01-17 07:50:45 -0800

    Revert "[JENKINS-72387] Provide JUnit5 Class (static) context for JenkinsRule"

--- src/main/java/org/jvnet/hudson/test/junit/jupiter/JUnit5JenkinsRule.java
+++ src/main/java/org/jvnet/hudson/test/junit/jupiter/JUnit5JenkinsRule.java
@@ -1,10 +1,9 @@
 package org.jvnet.hudson.test.junit.jupiter;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
-
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.runner.Description;
 import org.jvnet.hudson.test.JenkinsRecipe;
 import org.jvnet.hudson.test.JenkinsRule;
@@ -13,18 +12,18 @@ import org.jvnet.hudson.test.JenkinsRule;
  * Provides JUnit 5 compatibility for {@link JenkinsRule}.
  */
 class JUnit5JenkinsRule extends JenkinsRule {
+    private final ParameterContext context;
 
-    JUnit5JenkinsRule(@NonNull ExtensionContext extensionContext, Annotation... annotations) {
+    JUnit5JenkinsRule(@NonNull ParameterContext context, @NonNull ExtensionContext extensionContext) {
+        this.context = context;
         this.testDescription = Description.createTestDescription(
-            extensionContext.getTestClass().map(Class::getName).orElse(null),
-            extensionContext.getTestMethod().map(Method::getName).orElse(null),
-            annotations);
+                extensionContext.getTestClass().map(Class::getName).orElse(null),
+                extensionContext.getTestMethod().map(Method::getName).orElse(null));
     }
 
     @Override
     public void recipe() throws Exception {
-        final JenkinsRecipe jenkinsRecipe = this.testDescription.getAnnotation(JenkinsRecipe.class);
-
+        JenkinsRecipe jenkinsRecipe = context.findAnnotation(JenkinsRecipe.class).orElse(null);
         if (jenkinsRecipe == null) {
             return;
         }
