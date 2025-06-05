33074642785adc799749482f92c2b239c4692c89
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2025-06-05 15:12:10 +0200

    JUnit5 JenkinsRule supports timeout

---
+++ src/test/java/org/jvnet/hudson/test/junit/jupiter/timeout/JenkinsRuleTimeoutTestBase.java
@@ -0,0 +1,98 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.jvnet.hudson.test.junit.jupiter.timeout;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.*;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.TimeoutException;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+@ExtendWith(JenkinsRuleTimeoutTestBase.TimeoutExceptionValidator.class)
+@WithJenkins
+class JenkinsRuleTimeoutTestBase {
+
+    protected JenkinsRule r;
+
+    @BeforeEach
+    void setUp(JenkinsRule rule) {
+        r = rule;
+        r.timeout = 15; // let us not wait three minutes! but need enough time for Jenkins to start up, and then a few seconds more
+    }
+
+    protected static void hangInterruptibly() throws InterruptedException {
+        Thread.sleep(Long.MAX_VALUE);
+        fail("should not get here");
+    }
+
+    @SuppressWarnings("SleepWhileHoldingLock")
+    protected static void hangUninterruptibly() {
+        // Adapted from http://stackoverflow.com/a/22489064/12916
+        final Object a = new Object();
+        final Object b = new Object();
+        new Thread("other") {
+            @Override
+            public void run() {
+                synchronized (a) {
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException exc) {
+                        System.err.println("interrupted t1");
+                    }
+                    synchronized (b) {
+                        fail("should not get here");
+                    }
+                }
+            }
+        }.start();
+        synchronized (b) {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException exc) {
+                System.err.println("interrupted t2");
+            }
+            synchronized (a) {
+                fail("should not get here");
+            }
+        }
+    }
+
+    public static class TimeoutExceptionValidator implements InvocationInterceptor {
+
+        @Override
+        public void interceptTestMethod(Invocation<Void> invocation,
+                                        ReflectiveInvocationContext<Method> invocationContext,
+                                        ExtensionContext extensionContext) throws Throwable {
+            try {
+                invocation.proceed();
+            } catch (TimeoutException ex) {
+                System.out.println("Test failed with TimeoutException as expected");
+            }
+        }
+    }
+}
