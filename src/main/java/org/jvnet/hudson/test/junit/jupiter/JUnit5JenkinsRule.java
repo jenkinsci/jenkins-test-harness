97806ecd06f0049abbe2cadb4a453da1ed570611
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2025-12-20 12:35:16 +0100

    Simplifications for JenkinsExtension

--- src/main/java/org/jvnet/hudson/test/junit/jupiter/JUnit5JenkinsRule.java
+++ src/main/java/org/jvnet/hudson/test/junit/jupiter/JUnit5JenkinsRule.java
@@ -6,25 +6,23 @@ import java.lang.reflect.Method;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.runner.Description;
-import org.jvnet.hudson.test.JenkinsRecipe;
+import org.jvnet.hudson.test.HudsonHomeLoader;
 import org.jvnet.hudson.test.JenkinsRule;
-import org.jvnet.hudson.test.recipes.LocalData;
-import org.jvnet.hudson.test.recipes.WithPlugin;
-import org.jvnet.hudson.test.recipes.WithPluginManager;
 
 /**
  * Provides JUnit 5 compatibility for {@link JenkinsRule}.
  */
 class JUnit5JenkinsRule extends JenkinsRule {
-    private final ParameterContext context;
+    private final ParameterContext parameterContext;
     private final ExtensionContext extensionContext;
 
-    JUnit5JenkinsRule(@NonNull ParameterContext context, @NonNull ExtensionContext extensionContext) {
-        this.context = context;
+    JUnit5JenkinsRule(@NonNull ParameterContext parameterContext, @NonNull ExtensionContext extensionContext) {
+        this.parameterContext = parameterContext;
         this.extensionContext = extensionContext;
         this.testDescription = Description.createTestDescription(
                 extensionContext.getTestClass().map(Class::getName).orElse(null),
-                extensionContext.getTestMethod().map(Method::getName).orElse(null));
+                extensionContext.getTestMethod().map(Method::getName).orElse(null),
+                extensionContext.getTestMethod().map(Method::getAnnotations).orElse(null));
     }
 
     @Override
@@ -32,41 +30,18 @@ class JUnit5JenkinsRule extends JenkinsRule {
         // so that test code has all the access to the system
         ACL.as2(ACL.SYSTEM2);
 
-        JenkinsRecipe jenkinsRecipe =
-                context.findAnnotation(JenkinsRecipe.class).orElse(null);
-        if (jenkinsRecipe != null) {
-            @SuppressWarnings("unchecked")
-            final JenkinsRecipe.Runner<JenkinsRecipe> runner = (JenkinsRecipe.Runner<JenkinsRecipe>)
-                    jenkinsRecipe.value().getDeclaredConstructor().newInstance();
-            recipes.add(runner);
-            tearDowns.add(() -> runner.tearDown(this, jenkinsRecipe));
+        // WithLocalData does not implement JenkinsRecipe
+        WithLocalData withLocalData =
+                extensionContext.getTestMethod().orElseThrow().getAnnotation(WithLocalData.class);
+        if (withLocalData == null) {
+            Class<?> testClass = extensionContext.getTestClass().orElseThrow();
+            withLocalData = testClass.getAnnotation(WithLocalData.class);
+        }
+        if (withLocalData != null) {
+            with(new HudsonHomeLoader.Local(extensionContext.getTestMethod().orElseThrow(), withLocalData.value()));
         }
 
-        Method testMethod = extensionContext.getTestMethod().orElse(null);
-        if (testMethod != null) {
-            LocalData localData = testMethod.getAnnotation(LocalData.class);
-            if (localData != null) {
-                final JenkinsRecipe.Runner<LocalData> runner = new LocalData.RuleRunnerImpl();
-                recipes.add(runner);
-                runner.setup(this, localData);
-                tearDowns.add(() -> runner.tearDown(this, localData));
-            }
-
-            WithPlugin withPlugin = testMethod.getAnnotation(WithPlugin.class);
-            if (withPlugin != null) {
-                final JenkinsRecipe.Runner<WithPlugin> runner = new WithPlugin.RuleRunnerImpl();
-                recipes.add(runner);
-                runner.setup(this, withPlugin);
-                tearDowns.add(() -> runner.tearDown(this, withPlugin));
-            }
-
-            WithPluginManager withPluginManager = testMethod.getAnnotation(WithPluginManager.class);
-            if (withPluginManager != null) {
-                final JenkinsRecipe.Runner<WithPluginManager> runner = new WithPluginManager.RuleRunnerImpl();
-                recipes.add(runner);
-                runner.setup(this, withPluginManager);
-                tearDowns.add(() -> runner.tearDown(this, withPluginManager));
-            }
-        }
+        // other JenkinsRecipes handled here
+        super.recipe();
     }
 }
--- src/main/java/org/jvnet/hudson/test/junit/jupiter/JenkinsExtension.java
+++ src/main/java/org/jvnet/hudson/test/junit/jupiter/JenkinsExtension.java
@@ -1,12 +1,11 @@
 package org.jvnet.hudson.test.junit.jupiter;
 
-import java.lang.reflect.Method;
+import edu.umd.cs.findbugs.annotations.NonNull;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
 import org.junit.jupiter.api.extension.ParameterResolver;
-import org.jvnet.hudson.test.HudsonHomeLoader.Local;
 import org.jvnet.hudson.test.JenkinsRule;
 
 /**
@@ -14,49 +13,35 @@ import org.jvnet.hudson.test.JenkinsRule;
  *
  * @see WithJenkins
  */
-class JenkinsExtension implements ParameterResolver, AfterEachCallback {
+public class JenkinsExtension implements ParameterResolver, AfterEachCallback {
 
     private static final String KEY = "jenkins-instance";
     private static final ExtensionContext.Namespace NAMESPACE =
             ExtensionContext.Namespace.create(JenkinsExtension.class);
 
     @Override
-    public void afterEach(ExtensionContext context) throws Exception {
+    public void afterEach(@NonNull ExtensionContext context) throws Exception {
         final JenkinsRule rule = context.getStore(NAMESPACE).remove(KEY, JenkinsRule.class);
-        if (rule == null) {
-            return;
+        if (rule != null) {
+            rule.after();
         }
-        rule.after();
     }
 
     @Override
-    public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
+    public boolean supportsParameter(
+            @NonNull ParameterContext parameterContext, @NonNull ExtensionContext extensionContext)
             throws ParameterResolutionException {
         return parameterContext.getParameter().getType().equals(JenkinsRule.class);
     }
 
     @Override
-    public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext)
+    public Object resolveParameter(
+            @NonNull ParameterContext parameterContext, @NonNull ExtensionContext extensionContext)
             throws ParameterResolutionException {
         final JenkinsRule rule = extensionContext
                 .getStore(NAMESPACE)
                 .getOrComputeIfAbsent(
                         KEY, key -> new JUnit5JenkinsRule(parameterContext, extensionContext), JenkinsRule.class);
-
-        if (extensionContext.getTestMethod().isPresent()) {
-            // check for a WithLocalData annotation to set up JENKINS_HOME
-            Method testMethod = extensionContext.getTestMethod().get();
-            WithLocalData localData = testMethod.getAnnotation(WithLocalData.class);
-            if (localData == null && extensionContext.getTestClass().isPresent()) {
-                Class<?> testClass = extensionContext.getTestClass().get();
-                localData = testClass.getAnnotation(WithLocalData.class);
-            }
-
-            if (localData != null) {
-                rule.with(new Local(testMethod, localData.value()));
-            }
-        }
-
         try {
             rule.before();
             return rule;
